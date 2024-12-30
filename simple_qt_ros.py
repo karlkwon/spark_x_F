19b3b12a219a69ace725b878ceaba49e6afbe8ba
mrthink <mrthinks@gmail.com>
2024-12-30 14:29:11 +0900

    Add files via upload
    add multi-thread example

--- simple_qt_ros.py
+++ simple_qt_ros.py
@@ -9,12 +9,16 @@ from rclpy.node import Node
 from sensor_msgs.msg import CompressedImage
 import numpy as np
 
+import queue
+import time
+from threading import Thread
+
 
 form_class = uic.loadUiType("simple_qt_ros.ui")[0]
 
 
 class WindowClass(QMainWindow, form_class):
-	def __init__(self):
+	def __init__(self, operation_queue):
 		super().__init__()
 		self.setupUi(self)
 
@@ -23,6 +27,7 @@ class WindowClass(QMainWindow, form_class):
 		# self.label_1.setText('HAHA')
 
 		self.isClosed = False
+		self.operation_queue = operation_queue
 
 	def button1Function(self):
 		print("btn_1 clicked")
@@ -32,6 +37,9 @@ class WindowClass(QMainWindow, form_class):
 		# image = cv2.imread('SavedImage.jpg')
 		# image = self.cvimage_to_label(image)
 		# self.label_1.setPixmap(QPixmap.fromImage(image))
+
+	def editAddFunction(self, text):
+		self.edit_1.append(text)
 
 	def showImage(self, image_np):
 		image = self.cvimage_to_label(image_np)
@@ -48,7 +56,13 @@ class WindowClass(QMainWindow, form_class):
 		return image
 
 	def closeEvent(self, event):
+		self.operation_queue.put(' ')
+		self.operation_queue.put(' ')
+
 		print("closeEvent")
+
+		time.sleep(0.1)
+
 		self.isClosed = True
 
 
@@ -70,16 +84,60 @@ class ImageSubscriber(Node):
         # cv2.waitKey(1)  # Display the image until a keypress
 
 
+def ros_thread(operation_queue, event_queue, sleep_time):
+	time_p = time.time()
+
+	while(True):
+		try:
+			d = operation_queue.get_nowait()
+			if d is not None:
+				break
+		except queue.Empty:
+			pass
+
+		time_c = time.time()
+		if (time_c - time_p) > (sleep_time):
+			event_queue.put('sleep_time: ', sleep_time)
+			time_p = time_c
+
+		time.sleep(0.01)
+		# time.sleep(sleep_time)
+
+	print('exit : ', sleep_time)
+
+
 if __name__ == "__main__":
 	app = QApplication(sys.argv)
-	myWindow = WindowClass()
+	operation_queue = queue.Queue()
+	myWindow = WindowClass(operation_queue)
 
 	rclpy.init(args=None)
 	image_subscriber = ImageSubscriber()
 
 	myWindow.show()
 
+	event_queue1 = queue.Queue()
+	Thread(target = ros_thread, args=(operation_queue, event_queue1, 1), daemon=True).start()
+	event_queue2 = queue.Queue()
+	Thread(target = ros_thread, args=(operation_queue, event_queue2, 2), daemon=True).start()
+
+
 	while(myWindow.isClosed == False):
+
+		try:
+			d1 = event_queue1.get_nowait()
+			if d1 is not None:
+				myWindow.editAddFunction('1' + d1)
+		except queue.Empty:
+			pass
+
+		try:
+			d2 = event_queue2.get_nowait()
+			if d2 is not None:
+				myWindow.editAddFunction('2' + d2)
+		except queue.Empty:
+			pass
+
 		rclpy.spin_once(image_subscriber, timeout_sec=0)
 		if(image_subscriber.image_np is not None):
 			myWindow.showImage(image_subscriber.image_np)
